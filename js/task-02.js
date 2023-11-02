b381ba731d3623a8d880cee1777e07db8b752933
Rostyslav Protsenko <rostifaner@gmail.com>
2023-11-02 03:36:44 +0200

    Add Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+ingredients.forEach((ingredient) => {
+  const createList = document.createElement("li");
+  createList.textContent = ingredient;
+  createList.classList.add("item");
+  // console.log(createList);
+  const addList = document.querySelector("#ingredients");
+  addList.append(createList);
+});
