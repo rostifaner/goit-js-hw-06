ece1e8f6016f053e84a9e3d393d461ea4517131f
Rostyslav Protsenko <rostifaner@gmail.com>
2023-11-04 03:23:31 +0200

    Add task 4

---
+++ js/task-04.js
@@ -0,0 +1,12 @@
+const decBtn = document.querySelector('button[data-action="decrement"]');
+const incBtn = document.querySelector(`button[data-action="increment"]`);
+const counter = document.getElementById("value");
+let count = 0;
+decBtn.addEventListener("click", () => {
+  count += 1;
+  counter.textContent = count;
+});
+incBtn.addEventListener("click", () => {
+  count -= 1;
+  counter.textContent = count;
+});
