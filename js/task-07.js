853e46eee1d336a0d494c7f99091f61d486dee2f
Rostyslav Protsenko <rostifaner@gmail.com>
2023-11-06 21:57:20 +0200

    Add task 7

---
+++ js/task-07.js
@@ -0,0 +1,5 @@
+const inputEl = document.getElementById("font-size-control");
+const textEl = document.getElementById("text");
+inputEl.addEventListener("input", () => {
+  textEl.style.fontSize = inputEl.value + "px";
+});
