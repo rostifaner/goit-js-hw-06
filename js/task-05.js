5bfa96897468b568aad0cd27f1a0d68e8f5de42a
Rostyslav Protsenko <rostifaner@gmail.com>
2023-11-06 20:31:38 +0200

    Add task 5

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+input.addEventListener("input", handleEvent);
+function handleEvent(event) {
+  console.log(event.target.value);
+  const inputValue = event.target.value;
+  if (inputValue === "") {
+    output.textContent = "Anonymous";
+  } else {
+    output.textContent = inputValue;
+  }
+}
