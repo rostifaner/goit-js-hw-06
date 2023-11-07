74f703954920e4dff0f8e5c4de256a75d3a58ba0
Rostyslav Protsenko <rostifaner@gmail.com>
2023-11-07 03:00:58 +0200

    Add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnEl = document.querySelector(".change-color");
+const colorEl = document.querySelector("body");
+const text = document.querySelector(".color");
+btnEl.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  console.log(randomColor);
+  colorEl.style.backgroundColor = randomColor;
+  text.textContent = randomColor;
+});
