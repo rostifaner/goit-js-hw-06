62cca6181d04c3f4d8ee458352a16bd98f5c152b
Rostyslav Protsenko <rostifaner@gmail.com>
2023-11-07 02:22:59 +0200

    Add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
-
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.email.value;
+  const pass = form.elements.password.value;
+  if (login === "" || pass === "") {
+    alert("All fields must be filled!");
+  } else {
+    const value = {
+      email: login,
+      password: pass,
+    };
+    console.log(value);
+  }
+  form.reset();
+});
